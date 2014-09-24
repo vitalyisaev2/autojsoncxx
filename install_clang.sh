e6ad95a7090b65e89490fd3a033d40edeb47e91a
netheril96 <netheril96@gmail.com>
2014-09-24 16:48:38 +0800

    Why can't we debug travis offline?

--- install_clang.sh
+++ install_clang.sh
@@ -2,8 +2,8 @@
 
 wget -c 'http://llvm.org/releases/3.4.2/cfe-3.4.2.src.tar.gz'
 wget -c 'http://llvm.org/releases/3.4.2/llvm-3.4.2.src.tar.gz'
-tar xf 'llvm-3.4.2.src.tar.xz'
-tar xf 'cfe-3.4.2.src.tar.xz'
+tar xf 'llvm-3.4.2.src.tar.gz'
+tar xf 'cfe-3.4.2.src.tar.gz'
 mv 'cfe-3.4.2.src' 'llvm-3.4.2.src/tools/clang'
 mkdir 'build' && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../llvm-3.4.2.src/
